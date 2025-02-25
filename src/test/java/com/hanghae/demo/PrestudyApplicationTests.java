3bad2c38a97801d037374b64366d2168819a032a
DESKTOP-5JPIU0I\USER <repaid11010@naver.com>
2025-02-25 21:42:35 +0900

    init

---
+++ src/test/java/com/hanghae/demo/PrestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
